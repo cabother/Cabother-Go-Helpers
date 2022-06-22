078c54c8265d155bcc1b3a3dc24add03fa1f8bc3
Renato Soares <renato.f.soares@hotmail.com>
2022-06-22 11:44:41 -0300

    chore: change local from shs

--- scripts/mockery.sh
+++ scripts/mockery.sh
@@ -12,13 +12,3 @@ do
     mockery --all --recursive=true
     cd $rootPath
 done
-
-cont=0
-for filePath in $(find . -name "*.go")
-do
-    # echo $filePath | rev | cut -d"/" -f2- | rev
-    ((cont=$cont+1))
-    file_name="${filePath##*/}"
-    gofumpt -w $filePath
-    echo "$cont - $file_name -> DONE!"
-done
