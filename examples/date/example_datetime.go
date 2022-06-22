e3fc481ffb05b1bdd12104e5002992900dce7553
Renato Soares <renato.f.soares@hotmail.com>
2022-06-22 12:09:30 -0300

    chore: add example to use datetime

--- examples/date/example_datetime.go
+++ examples/date/example_datetime.go
@@ -1,1 +1,26 @@
 package date
+
+import (
+	"fmt"
+
+	pkgDate "github.com/cabother/cabother-go-helpers/pkg/date"
+)
+
+type DateTimeExampleInterface interface {
+	GetCurrentDateTime() error
+}
+
+type DateTimeExample struct{}
+
+func New() DateTimeExampleInterface {
+	return &DateTimeExample{}
+}
+
+func (a *DateTimeExample) GetCurrentDateTime() error {
+	instance := pkgDate.NewDatetime()
+	currentDateTime := instance.GetDateNow("dd/MM/yyyyT00:00:00")
+	//... your code here
+
+	fmt.Println(currentDateTime)
+	return nil
+}
