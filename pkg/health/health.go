78b52a1375d73ad7dacc5d0ad08fb80cb34ef058
Renato Soares <renato.f.soares@hotmail.com>
2025-01-09 10:51:24 -0300

    feat: added examples to swagger doc

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -3,6 +3,6 @@ package health
 // Entity
 // @Description Objeto de resposta para validação de saúde da aplicação
 type Entity struct {
-	Hostname string `json:"hostname,omitempty"`
-	IP       string `json:"ip,omitempty"`
+	Hostname string `json:"hostname,omitempty" example:"server-linux"`
+	IP       string `json:"ip,omitempty" example:"192.168.0.1"`
 } // @name Entity
